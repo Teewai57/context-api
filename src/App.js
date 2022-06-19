5d02573f0cdf473cc2e30503ca233297073b423a
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-19 22:34:45 +0100

    usetate in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Login from "./components/login"
+import React, { useState } from "react"
 
 export default function App() {
+  const[showProfile, setShowProfile] = useState(false);
+
   return(
     <div className="App">
           <Login />
