cffe66b2bb6690fabda708dd9c5cd3b9471ed27d
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-19 22:23:49 +0100

    styling inputs

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+
 export default function Login() {
     const[username, setUsername] = useState("");
     const[showProfile, setShowProfile] = useState(false);
