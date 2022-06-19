9d0585428df5afbfed9dc242d3d40e364de27682
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-19 23:06:49 +0100

    done with app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Login from "./components/login"
 import Profile from "./components/profile";
 import React, { useState } from "react"
 import { LoginContext } from "./Contexts/loginContext";
+import "./App.css"
+
+
 export default function App() {
   const[showProfile, setShowProfile] = useState(false);
   const[username, setUsername] = useState("");
