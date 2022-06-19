369c1e202d1557ee6b3249049f30ddf51dc8b627
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-19 22:15:16 +0100

    added input for username

--- src/components/login.js
+++ src/components/login.js
@@ -3,4 +3,16 @@ import React, { useState } from "react";
 export default function Login() {
     const[username, setUsername] = useState("");
     const[showProfile, setShowProfile] = useState(false);
+
+    return(
+        <>
+        <input 
+        type="text"
+        placeholder="Username..."
+        onChange={(event) => {
+            setUsername(event.target.value);
+        }}
+        />
+        </>
+    )
 }
