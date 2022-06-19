7a6c434cfa2c09960ce315718b56b2e668bbd607
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-19 22:43:21 +0100

    wrapping with context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Login from "./components/login"
 import Profile from "./components/profile";
 import React, { useState } from "react"
-
+import { LoginContext } from "./Contexts/loginContect";
 export default function App() {
   const[showProfile, setShowProfile] = useState(false);
 
   return(
     <div className="App">
+      <LoginContext>
       {showProfile ? <Profile /> : <Login />}
-          <Login />
+      </LoginContext>
     </div>
   )
 }
